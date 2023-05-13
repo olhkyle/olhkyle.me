3e05c0302b02a059382027713c2f9f83859a97d2
olhkyle <kylekwon96@gmail.com>
2023-05-13 23:12:42 +0900

    [STYLE] Layout 컴포넌트 스타일링

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Footer, Nav, ScrollToTopButton } from '.';
+import { Nav, ScrollToTopButton, ThemeButton } from '.';
 import { Outlet } from 'react-router-dom';
+import useSideNavActive from '../hooks/useSideNavActive';
 
 const Layout = () => {
+  const [isActive] = useSideNavActive();
+
   return (
-    <>
+    <div className="flex w-screen h-screen">
       <Nav />
-      <Outlet />
-      <Footer />
+      <main
+        className={`mx-[100px] p-[3rem] transition-transform ${
+          isActive ? 'translate-x-[360px]' : 'translate-x-[72px]'
+        } duration-500 lg:max-w-5xl`}>
+        <Outlet />
+      </main>
       <ScrollToTopButton />
-    </>
+      <ThemeButton />
+    </div>
   );
 };
 
