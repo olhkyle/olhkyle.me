264474f902d08be344b5837ee72fb58fbafc37e3
olhkyle <kylekwon96@gmail.com>
2023-05-13 11:33:12 +0900

    [STYLE] css 변수 추가 및 dark mode 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: 'var(--color-black)',
+        white: 'var(--color-white)',
+        gray: {
+          100: 'var(--color-gray-100)',
+          200: 'var(--color-gray-200)',
+          300: 'var(--color-gray-300)',
+          400: 'var(--color-gray-400)',
+          500: 'var(--color-gray-500)',
+          600: 'var(--color-gray-600)',
+          700: 'var(--color-gray-700)',
+          800: 'var(--color-gray-800)',
+          900: 'var(--color-gray-900)',
+        },
+        green: {
+          100: 'var(--color-green-100)',
+          200: 'var(--color-green-200)',
+          300: 'var(--color-green-300)',
+          400: 'var(--color-green-400)',
+        },
+        blue: {
+          100: 'var(--color-blue-100)',
+          200: 'var(--color-blue-200)',
+        },
+        purple: 'var(--color-purple)',
+        orange: {
+          100: 'var(--color-orange-100)',
+          200: 'var(--color-orange-200)',
+        },
+        red: 'var(--color-red)',
+        dark: 'var(--color-dark)',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
