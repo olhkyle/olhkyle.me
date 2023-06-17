17abbae08b4d798051933e0f8612004521bfcfe9
olhkyle <kylekwon96@gmail.com>
2023-06-18 00:21:43 +0900

    [STYLE] layout 관련 문제 해결

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,15 +7,15 @@ const Layout = () => {
 	const [isActive] = useSideNavActive();
 
 	return (
-		<div className="flex w-screen h-screen">
+		<div className="flex">
 			<Nav />
 			<main
-				className={`ml-[40px] mr-[80px] mt-[1rem] p-[3rem] w-full h-full transition-transform overflow-scroll hide-scrollbar hide-scrollbar::-webkit-scrollbar ${
+				className={`m-[3rem] pl-[2rem] pt-[2rem] w-full h-full transition-transform ${
 					isActive ? 'translate-x-[360px]' : 'translate-x-[72px]'
-				} duration-500 lg:max-w-6xl lg:mx-[120px] md:min-w-[600px] sm:min-w-[300px]`}>
+				} duration-500 lg:max-w-6xl`}>
 				<Outlet />
 			</main>
-			<ScrollToTopButton />
+			<ScrollToTopButton topPosToStopShowing={300} />
 			<ThemeButton />
 		</div>
 	);
