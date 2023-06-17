a2b8dd7c9e5bb0b316d1417cc44850322fa1d639
olhkyle <kylekwon96@gmail.com>
2023-06-18 00:22:28 +0900

    [STYLE] MainTitle 스타일 수정

--- src/components/common/MainTitle.tsx
+++ src/components/common/MainTitle.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 interface TitleProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const Title = ({ children }: TitleProps) => {
-  return <h1 className="text-6xl font-bold lg:text-8xl break-keep">{children}</h1>;
+	return <h1 className="text-6xl font-bold break-keep lg:text-8xl sm:text-2xl">{children}</h1>;
 };
 
 export default Title;
