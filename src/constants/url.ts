f8baf9aae3e803e1cc6f5ed88bd9ba45e15a4336
olhkyle <kylekwon96@gmail.com>
2023-06-21 02:25:47 +0900

    [-] add url constant

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,3 +4,7 @@ export const DESIGNTHOU = 'https://designthou.com/';
 export const CLASS101 = 'https://class101.net/en/products/604c99bfc4b8930014e277f9';
 export const LINKEDIN = 'https://www.linkedin.com/in/hyukmin-kwon';
 export const VELOG = 'https://velog.io/@defaultkyle/series';
+export const FINEAPPLE_GITHUB = 'https://github.com/JP-s-Children/FineApple';
+export const FINEAPPLE_DEPLOY = 'https://fineapple-ea261.web.app';
+export const FINEAPPLE_RETROSPECT =
+	'https://sumz.notion.site/Suspense-Option-in-AuthenticationGuard-HOC-cc74b24870004a78861f5b7d9f16ba5b?pvs=4';
