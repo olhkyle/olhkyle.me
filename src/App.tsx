92932dd49b9144f1b5b758233bd8f7394da769ce
olhkyle <kylekwon96@gmail.com>
2023-06-21 00:05:55 +0900

    [FEAT] adapt mobile view height

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,13 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
+	const setScreenSize = () => {
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	};
+
+	window.addEventListener('resize', setScreenSize);
+
 	return (
 		<RecoilRoot>
 			<RouterProvider router={router} />
