42a47e9af05bc22fb723a0e42b44e9616cc979b4
olhkyle <kylekwon96@gmail.com>
2023-06-22 01:38:32 +0900

    [FEAT] implement Footer component

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import SocialBadge from './SocialBadge';
+import { GITHUB, LINKEDIN, VELOG } from '../../constants/url';
+import { AiOutlineGithub, AiOutlineLink } from 'react-icons/ai';
+import { FiLinkedin } from 'react-icons/fi';
+
+interface SocialLink {
+	name: string;
+	href: string;
+	icon: React.ReactElement;
+}
+
+const socialLinks: SocialLink[] = [
+	{ name: 'github', href: GITHUB, icon: <AiOutlineGithub size="24" /> },
+	{
+		name: 'linkedin',
+		href: LINKEDIN,
+		icon: <FiLinkedin size="24" />,
+	},
+	{ name: 'blog', href: VELOG, icon: <AiOutlineLink size="24" /> },
+];
+
+const Footer = () => {
+	return (
+		<footer className="mt-20 px-[1rem]">
+			<div className="flex justify-between items-center mx-auto py-5 border-t-[1px] font-medium border-gray-200 md:w-[768px] lg:w-[1280px]">
+				<div className="flex gap-2">
+					<span>© 2023</span>
+					<span>💿lhkyle.</span>
+				</div>
+				<ul className="flex items-center gap-3">
+					{socialLinks.map(({ name, href, icon }) => (
+						<li key={name}>
+							<SocialBadge linkHref={href}>{icon}</SocialBadge>
+						</li>
+					))}
+				</ul>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
