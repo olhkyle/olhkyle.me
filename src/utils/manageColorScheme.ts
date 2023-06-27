8e4e0bde4fa982cb065433584dd0dd975e00acf3
olhkyle <kylekwon96@gmail.com>
2023-06-27 23:23:32 +0900

    [FEAT] update manageColorScheme function

--- src/utils/manageColorScheme.ts
+++ src/utils/manageColorScheme.ts
@@ -1,6 +1,7 @@
 const backEndStacks = ['Node.js', 'Express.js'];
 const designTools: string[] = ['Figma', 'Photoshop', 'Illustrator', 'Premiere Pro'];
 const collaborationTools: string[] = ['Git', 'Github', 'Slack'];
+const deploymentTools: string[] = ['Github Actions', 'AWS Cloudfront/S3', 'Firebase'];
 
 const manageColorScheme = (skill: string): string => {
 	if (backEndStacks.includes(skill)) {
@@ -9,6 +10,8 @@ const manageColorScheme = (skill: string): string => {
 		return 'text-orange-200';
 	} else if (designTools.includes(skill)) {
 		return 'text-purple';
+	} else if (deploymentTools.includes(skill)) {
+		return 'text-gray-600';
 	} else {
 		return 'text-blue-200';
 	}
