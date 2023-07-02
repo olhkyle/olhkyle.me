5abaf9ee1563503f395eccf3e7a73ca3e7969100
olhkyle <kylekwon96@gmail.com>
2023-07-03 01:38:44 +0900

    [REFAC] remove unnecessary type assertion

--- src/components/common/LanguageSelect.tsx
+++ src/components/common/LanguageSelect.tsx
@@ -16,7 +16,7 @@ const LanguageSelect = ({ currentLang, setCurrentLang }: LanguageSelectProps) =>
 					className={`${
 						currentLang === lang ? 'font-bold toggle-dark' : 'font-normal'
 					} px-2 py-1 rounded-lg cursor-pointer border-[1px] dark:border-gray-200 sm:px-4 sm:py-2`}
-					onClick={() => setCurrentLang(lang as Lang)}>
+					onClick={() => setCurrentLang(lang)}>
 					{lang}
 				</li>
 			))}
