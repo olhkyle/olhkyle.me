2964b15d35495538e6d26a2da706846af1f2216e
olhkyle <kylekwon96@gmail.com>
2023-07-03 01:35:53 +0900

    [REFAC] refactor formattedDate function

--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -1,11 +1,14 @@
 const insertStringZero = (stringifyDate: string) => stringifyDate.padStart(2, '0');
 const isString = (date: Date | string) => typeof date === 'string';
 
-const formattedDate = ({ startDate, endDate }: { startDate: Date; endDate: Date | string }) =>
-	`${startDate.getFullYear()}.${insertStringZero(startDate.getMonth() + '')} ${
-		isString(endDate) && endDate === '' ? '' : '-'
-	} ${isString(endDate) ? endDate : (endDate as Date).getFullYear()}${isString(endDate) ? '' : '.'}${
+const formattedDate = ({ startDate, endDate }: { startDate: Date; endDate: Date | string }) => {
+	const start = `${startDate.getFullYear()}.${insertStringZero(startDate.getMonth() + '')}`;
+
+	const end = `${isString(endDate) ? endDate : (endDate as Date).getFullYear()}${isString(endDate) ? '' : '.'}${
 		isString(endDate) ? '' : insertStringZero((endDate as Date).getMonth() + '')
 	}`;
+
+	return `${start} ${isString(endDate) && endDate === '' ? '' : '-'} ${end}`;
+};
 
 export default formattedDate;
